d5c1c0b3174f394960b4bfade9949549a521e760
iamshreeram <shreeram.v@live.com>
2017-10-03 19:41:03 -0600

    Classification of positive and negative feedback

--- main.py
+++ main.py
@@ -6,44 +6,73 @@
 ################################################
 ################################################
 
-from textblob import TextBlob
+import re
 import tweepy
-import os
+from tweepy import OAuthHandler
+from textblob import TextBlob
 
-def auth():
-    #Authentication
-    cons_key = os.environ.get('CONSUMER_KEY')
-    cons_sec = os.environ.get('CONSUMER_SECRET')
-    acces_tk = os.environ.get('ACCESS_TOKEN')
-    acces_tk_sec = os.environ.get('ACCESS_SECRET')
-
-    print(cons_key)
-    print(cons_sec)
-    print(acces_tk)
-    print(acces_tk_sec)
-
-
-    try:
-        auth = tweepy.OAuthHandler(cons_key,cons_sec)
-        auth.set_access_token(acces_tk,acces_tk_sec)
-        api = tweepy.API(auth)
-    except:
-        print("Error in authentication ")
-
-    return api
-
-def search(api):
-    #Searching
-    tweets = api.search('Trump')
-
-    for tweet in tweets:
-        print(tweet.text)
-        analysis = TextBlob(tweet.text)
-        print(analysis.sentiment)
-
+class TwitterClient(object):
+    def __init__(self):
+        consumer_key = os.environ.get('CONSUMER_KEY')
+        consumer_secret = os.environ.get('CONSUMER_SECRET')
+        access_token = os.environ.get('ACCESS_TOKEN')
+        access_token_secret = os.environ.get('ACCESS_SECRET')
+ 
+        try:
+            self.auth = OAuthHandler(consumer_key, consumer_secret)
+            self.auth.set_access_token(access_token, access_token_secret)
+            self.api = tweepy.API(self.auth)
+        except:
+            print("Error: Authentication Failed")
+ 
+    def clean_tweet(self, tweet):
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+ 
+    def get_tweet_sentiment(self, tweet):
+        analysis = TextBlob(self.clean_tweet(tweet))
+        if analysis.sentiment.polarity > 0:
+            return 'positive'
+        elif analysis.sentiment.polarity == 0:
+            return 'neutral'
+        else:
+            return 'negative'
+ 
+    def get_tweets(self, query, count = 10):
+        tweets = []
+        try:
+            fetched_tweets = self.api.search(q = query, count = count)
+            for tweet in fetched_tweets:
+                parsed_tweet = {}
+                parsed_tweet['text'] = tweet.text
+                parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
+                if tweet.retweet_count > 0:
+                    if parsed_tweet not in tweets:
+                        tweets.append(parsed_tweet)
+                else:
+                    tweets.append(parsed_tweet)
+            return tweets
+ 
+        except tweepy.TweepError as e:
+            print("Error : " + str(e))
+ 
 def main():
-    api = auth()
-    search(api)
-
-if __name__=="__main__":
+    api = TwitterClient()
+	queryDetails = raw_input("What do you want to query about : ")
+	if queryDetails ==""
+		queryDetails ='Python3'
+    tweets = api.get_tweets(query = queryDetails, count = 200)
+    ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
+    print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets)))
+    ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
+    print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets)))
+    print("Neutral tweets percentage: {} % \
+        ".format(100*len(tweets - ntweets - ptweets)/len(tweets)))
+    print("\n\nPositive tweets:")
+    for tweet in ptweets[:10]:
+        print(tweet['text'])
+    print("\n\nNegative tweets:")
+    for tweet in ntweets[:10]:
+        print(tweet['text'])
+ 
+if __name__ == "__main__":
     main()
